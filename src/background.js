1eb0d415a88a4b6f692e6fbd3cd0b66b8d031653
Federico Pasquarelli <p.relli_federico@hotmail.it>
2021-05-23 19:05:18 +0200

    optimize css for select2

--- src/background.js
+++ src/background.js
@@ -1,6 +1,12 @@
+let isActive = false;
 chrome.action.onClicked.addListener((tab) => {
+  isActive = !isActive;
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ["web.js"],
+  });
+  chrome.browserAction.setIcon({
+    path: isActive ? "/icons/icon128.png" : "/icons/icon128negative.png",
+    tabId: sender.tab.id,
   });
 });
