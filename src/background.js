2ffcf4c236c23959f0bc9f10c02d7396ab1a9e87
federicopasquarelli <p.relli_federico@hotmail.it>
2021-06-12 23:04:08 +0200

    fix

--- src/background.js
+++ src/background.js
@@ -1,6 +1,4 @@
-let isActive = false;
 chrome.action.onClicked.addListener((tab) => {
-  isActive = !isActive;
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ["web.js"],
